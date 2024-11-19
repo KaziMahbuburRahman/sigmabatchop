c6db194589fbb9fd814859278cce4c30cc626205
NoobMahbub <Mahbubbengali@gmail.com>
2024-11-19 22:32:20 +0600

    redux done

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, multiply } from './redux/counter/counterSlice';
+
+
+function App() {
+
+  const count = useSelector((state) => state.counter.value)
+ const dispatch = useDispatch()
+  return (
+    
+      <div className="App">
+      <Navbar/>
+      <button onClick={()=> dispatch(decrement())}>-</button>
+      Currently count is {count}
+      <button onClick={()=> dispatch(increment())}>+</button>
+      <button onClick={()=>dispatch(multiply())}>*</button>
+      </div>
+       
+
+
+  );
+}
+
+export default App;
