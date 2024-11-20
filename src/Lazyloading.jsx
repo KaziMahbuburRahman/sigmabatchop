0d938e071b3dbca64bf5350340eec19c0c7547be
NoobMahbub <Mahbubbengali@gmail.com>
2024-11-20 13:47:56 +0600

    lazy loading done

---
+++ src/Lazyloading.jsx
@@ -0,0 +1,43 @@
+import { lazy, Suspense, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+const DummyText = lazy(()=>import('./Fakedata'))
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+      <div className="App">
+      <Navbar/>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR updates.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more.
+      </p>
+      <Suspense fallback={"Loading.."}>
+        <DummyText />
+      </Suspense>
+    </div>
+    
+  );
+}
+
+export default App;
